c65135401bdcd13dfb647eea64093b643c734c30
Mayja Tom <maja.morales17@gmail.com>
2018-12-15 00:10:06 -0800

    adding dependencies and psuedo code

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+//adding dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+//configure connection with local host
+
+
+//grab a list of all items by creating new Table function expression
+//will need a for loop to push table info to new Table visible in command line
+
+//could inclue a switch case to handle the various functions
+
+//Prompt user with ID of product they want to buy
+//Second question is quantity
+//check against inventory numbers and either complete their order, or tell them it's not available/quantity insufficient
+
