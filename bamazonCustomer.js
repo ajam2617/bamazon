3b6489971e0ba60aa1ed639f9a50e61077449b0e
Mayja Tom <maja.morales17@gmail.com>
2018-12-15 16:18:03 -0800

    completed logic. Getting some errors when testing. Still making some changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,14 +4,108 @@ var inquirer = require("inquirer");
 var Table = require('cli-table');
 
 //configure connection with local host
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
 
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "emmysDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
 
 //grab a list of all items by creating new Table function expression
 //will need a for loop to push table info to new Table visible in command line
+//ask the user what they would like to do
+function preview() {
+    connection.query("SELECT * FROM nominees", function (err, res) {
+        var table = new Table({
+            chars: {
+                'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+                , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+                , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+                , 'right': '║', 'right-mid': '╢', 'middle': '│'
+            },
+            head: ["Item ID", "Product", "Department", "Price", "Stock"],
+            colWidths: [6, 30, 12, 15, 8]
+        });
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(table.toString());
+        restart();
+
+    });
+};
+
+//restart function
+function restart() {
+    inquirer.prompt([{
+        type: "list",
+        name: "continue",
+        choices: ["Yes", "No"],
+        message: "Would you like to shop some more?\n"
+    }]).then(function (answers) {
+
+        if (answers.continue === "Yes") {
+            manageItems();
+
+        } else {
+            exit()
+        }
+
+    });
+};
+
+//end function
+function exit() {
+    console.log("Thanks for shopping with bamazon!! Goodbye.")
+    //This will exit out of our command line process
+    connection.end()
+}
 
 //could inclue a switch case to handle the various functions
-
 //Prompt user with ID of product they want to buy
 //Second question is quantity
-//check against inventory numbers and either complete their order, or tell them it's not available/quantity insufficient
+function manageItems() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userItem",
+            message: "What is the ID of the item you would like to purchase?",
+
+        },
+        {
+            type: "input",
+            name: "userQuantity",
+            message: "How many items would you like?"
+        }
+    ]).then(function (answers) {
+        //check against inventory numbers and either complete their order, or tell them it's not available/quantity insufficient
+        var userChoiceId = parseInt(answers.userItem);
+        var userHowMany = parseInt(answers.userQuantity);
+        var stockupdate = userChoiceId.stock_quantity - userHowMany;
+        if (stockupdate >= 0) {
+            connection.query('UPDATE products SET ? WHERE item_id = ?', [{ stock_quantity: stockupdate }, userChoiceId]);
+            restart();
+        } else {
+            console.log("Insufficient Quantity")
+            restart();
+        }
+
+    })
+}
+
+preview();
+
+
+
 
--- bamazonCustomerSeed.sql
+++ bamazonCustomerSeed.sql
@@ -5,41 +5,41 @@ CREATE DATABASE bamazonDB;
 USE  bamazonDB;
 
 CREATE TABLE products (
-    item_id INT NOT NULL,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50),
     department_name VARCHAR(50),
     price DECIMAL(4,2),
     stock_quantity INT NOT NULL,
-    PRIMARY KEY (product_name)
+    PRIMARY KEY (item_id)
 );
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (629, "Giant Inflatable Pool Floatie", "Toys", 25.99, 22);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Giant Inflatable Pool Floatie", "Toys", 25.99, 22);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (878, "Apple Slicer", "Home", 7.99, 12);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Apple Slicer", "Home", 7.99, 12);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (1200, "Succulent Christmas Tree", "Garden", 8.99, 4);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Succulent Christmas Tree", "Garden", 8.99, 4);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (503, "Dog Sweater", "Pets", 23.99, 12);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Dog Sweater", "Pets", 23.99, 12);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (515, "Claw Caps for Cats", "Pets", 10.58, 6);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Claw Caps for Cats", "Pets", 10.58, 6);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (900, "Yoga Cat Calendar", "Home", 11.53, 2);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Yoga Cat Calendar", "Home", 11.53, 2);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (1020, "White Board", "Office", 25.30, 8);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("White Board", "Office", 25.30, 8);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (725, "Sounds Machine", "Toys", 11.99, 8);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Sounds Machine", "Toys", 11.99, 8);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (223, "Unicorn Snot", "Cosmetics", 12.99, 4);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Unicorn Snot", "Cosmetics", 12.99, 4);
 
-INSERT INTO products(item_id, product_name, department_name, price,stock_quantity)
-VALUES (151, "Inflatable T-Rex Costume", "Costumes", 48.83, 4);
+INSERT INTO products(product_name, department_name, price,stock_quantity)
+VALUES ("Inflatable T-Rex Costume", "Costumes", 48.83, 4);
 
